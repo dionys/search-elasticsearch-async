9d637a86c503fa2dd757d0e55aed3bdb865dcb32
Denis Ibaev <dionys@gmail.com>
2014-03-28 01:34:34 +0400

    Added roles to provide common async client functionality

---
+++ lib/Search/Elasticsearch/Role/Client/Async.pm
@@ -0,0 +1,20 @@
+package Search::Elasticsearch::Role::Client::Async;
+
+use Moo::Role;
+
+with 'Search::Elasticsearch::Role::Client';
+
+
+sub perform_request {
+	my $self = shift();
+	my $cb   = pop();
+
+	my $req = $self->parse_request(@_);
+
+	$self->transport->perform_request($req, $cb);
+
+	return;
+}
+
+
+1;
---
+++ lib/Search/Elasticsearch/Role/Client/Direct/Async.pm
@@ -0,0 +1,37 @@
+package Search::Elasticsearch::Role::Client::Direct::Async;
+
+use Moo::Role;
+
+use Package::Stash ();
+
+with 'Search::Elasticsearch::Role::Client::Async';
+with 'Search::Elasticsearch::Role::Client::Direct';
+
+
+sub _install_api {
+	my ($cls, $grp) = @_;
+
+	my $defs = $cls->api;
+	my $pkg  = Package::Stash->new($cls);
+
+	my $re = $grp ? qr/$grp\./ : qr//;
+
+	for my $act (keys(%$defs)) {
+		my ($name) = $act =~ /^$re([^.]+)$/;
+
+		next unless $name;
+		next if $pkg->has_symbol('&' . $name);
+
+		my %def = (name => $name, %{$defs->{$act}});
+
+		$pkg->add_symbol(
+			'&' . $name => sub {
+				shift()->perform_request(\%def, @_);
+				return;
+			}
+		);
+	}
+}
+
+
+1;
