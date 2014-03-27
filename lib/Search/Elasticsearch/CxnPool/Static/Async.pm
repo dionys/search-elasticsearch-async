8be37de5136a9b37bb730c6a3c6d01899070d8e5
Denis Ibaev <dionys@gmail.com>
2014-03-28 01:36:57 +0400

    Added base async connection pool class

---
+++ lib/Search/Elasticsearch/CxnPool/Static/Async.pm
@@ -0,0 +1,59 @@
+package Search::Elasticsearch::CxnPool::Static::Async;
+
+use Moo;
+
+with 'Search::Elasticsearch::Role::CxnPool::Static',
+     'Search::Elasticsearch::Role::Is_Async';
+
+use Search::Elasticsearch::Util qw(new_error);
+
+use namespace::clean;
+
+
+sub next_cxn {
+	my ($self, $cb) = @_;
+
+	my $cxns = $self->cxns;
+	my (%seen, @skip);
+
+	my $sub; $sub = sub {
+		my $cnt = @$cxns;
+		my $fnd;
+
+		if ($cnt > keys(%seen)) {
+			while ($cnt--) {
+				my $cxn = $cxns->[$self->next_cxn_num];
+
+				next if $seen{$cxn}++;
+
+				return $cb->($cxn) if $cxn->is_live;
+
+				if ($cxn->next_ping <= time()) {
+					$fnd = $cxn;
+					last;
+				}
+				push(@skip, $cxn);
+			}
+		}
+
+		if ($fnd //= shift(@skip)) {
+			return $fnd->pings_ok(sub {
+				return $cb->($fnd) if $_[0];
+				$sub->();
+			});
+		}
+
+		$_->force_ping() for @$cxns;
+
+		local $@ = new_error('NoNodes', 'No nodes are available: [' . $self->cxns_str . ']');
+
+		$cb->();
+	};
+
+	$sub->();
+
+	return;
+}
+
+
+1;
