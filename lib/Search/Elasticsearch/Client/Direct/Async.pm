1a3bdff7be51a70d50e2ae8102314b8edddb30b8
Denis Ibaev <dionys@gmail.com>
2014-03-28 01:38:08 +0400

    Added base async client class

---
+++ lib/Search/Elasticsearch/Client/Direct/Async.pm
@@ -0,0 +1,11 @@
+package Search::Elasticsearch::Client::Direct::Async;
+
+use Moo;
+
+with    'Search::Elasticsearch::Role::API',
+        'Search::Elasticsearch::Role::Client::Direct::Async',
+        'Search::Elasticsearch::Role::Is_Async';
+extends 'Search::Elasticsearch::Client::Direct';
+
+
+1;
