7adee2c8d3f860942ba90198c7a680ac1e464987
Denis Ibaev <dionys@gmail.com>
2014-03-31 18:38:45 +0400

    Added async transport class

---
+++ lib/Search/Elasticsearch/Transport/Async.pm
@@ -0,0 +1,66 @@
+package Search::Elasticsearch::Transport::Async;
+
+use Moo;
+
+use Time::HiRes qw(time);
+
+use Search::Elasticsearch::Util qw(upgrade_error);
+
+with 'Search::Elasticsearch::Role::Transport',
+     'Search::Elasticsearch::Role::Is_Async';
+
+
+sub perform_request {
+	my $self   = shift();
+	my $cb     = pop();
+
+	my $pars = $self->tidy_request(@_);
+	my $pool = $self->cxn_pool;
+	my $log  = $self->logger;
+
+	$pool->next_cxn(sub {
+		unless (@_) {
+			$log->error($@);
+			$cb->();
+
+			return;
+		}
+
+		my ($cxn) = @_;
+		my $ts = time();
+
+		$log->trace_request($cxn, $pars);
+		$cxn->perform_request($pars, sub {
+			if (@_) {
+				my ($code, $res) = @_;
+
+				$pool->request_ok($cxn);
+				$log->trace_response($cxn, $code, $res, time() - $ts);
+				$cb->($res);
+			}
+			else {
+				my $err = upgrade_error($@, {request => $pars});
+
+				if ($pool->request_failed($cxn, $err)) {
+					$log->debugf("[%s] %s", $cxn->stringify(), "$err");
+					$log->info('Retrying request on a new cxn');
+
+					return $self->perform_request($pars, $cb);
+				}
+
+				$log->trace_error($cxn, $err);
+				delete($err->{vars}{body});
+				$err->is('NoNodes')
+					? $log->critical($err)
+					: $log->error($err);
+
+				$cb->();
+			}
+		});
+	});
+
+	return;
+}
+
+
+1;
